10c8a1e0cf6569d1565cb2646e11a7ae1051b2dd
Dorian Zedler <dorian@itsblue.de>
2021-04-30 20:54:22 +0200

    Added linbofs from @fshuett

--- board/linbo7-x86-64/post_build_put.sh
+++ board/linbo7-x86-64/post_build_put.sh
@@ -10,10 +10,10 @@ mkdir -p "${TARGET_DIR}"/usr/share/udhcpc/default.script.d
 mkdir -p "${TARGET_DIR}"/usr/share/grub
 
 # copy templates to target
-cp -v "${BASE_DIR}"/../../share/templates/grub.cfg.local.oss "${TARGET_DIR}"/usr/share/grub/grub.cfg
+#cp -v "${BASE_DIR}"/../../share/templates/grub.cfg.local.oss "${TARGET_DIR}"/usr/share/grub/grub.cfg
 # copy linbofs files to target
-LINBOFS_DIR="${BASE_DIR}"/../../linbofs
-cp -v "${LINBOFS_DIR}"/etc/linbo-version "${TARGET_DIR}"/etc/
+LINBOFS_DIR="${BR2_EXTERNAL}"/linbofs
+#cp -v "${LINBOFS_DIR}"/etc/linbo-version "${TARGET_DIR}"/etc/
 cp -v "${LINBOFS_DIR}"/etc/rsync.exclude "${TARGET_DIR}"/etc/
 cp -v "${LINBOFS_DIR}"/etc/newdev-patch.bvi "${TARGET_DIR}"/etc/newdev-patch.bvi
 cp -v "${LINBOFS_DIR}"/bin/patch_registry "${TARGET_DIR}"/usr/bin/
@@ -24,7 +24,7 @@ cp -v "${LINBOFS_DIR}"/usr/bin/linbo_wrapper "${TARGET_DIR}"/usr/bin/linbo_wrapp
 sed 's@#!/bin/sh@#!/usr/bin/ash@' -i "${TARGET_DIR}"/usr/bin/linbo_cmd
 sed 's@#!/bin/sh@#!/usr/bin/ash@' -i "${TARGET_DIR}"/usr/bin/linbo_wrapper
 # copy linbo files to target
-LINBO_DIR="${BASE_DIR}"/../../linbo
-cp -v "${LINBO_DIR}"/icons/linbo_wallpaper_1024x768.png "${TARGET_DIR}"/icons/linbo_wallpaper.png
-cp -v "${LINBO_DIR}"/linuxmuster-win/* "${TARGET_DIR}"/linuxmuster-win/
-cp -v "${LINBO_DIR}"/german.kbd "${TARGET_DIR}"/usr/share/de-latin1-nodeadkeys.kmap
+#LINBO_DIR="${BASE_DIR}"/../../linbo
+#cp -v "${LINBO_DIR}"/icons/linbo_wallpaper_1024x768.png "${TARGET_DIR}"/icons/linbo_wallpaper.png
+#cp -v "${LINBO_DIR}"/linuxmuster-win/* "${TARGET_DIR}"/linuxmuster-win/
+#cp -v "${LINBO_DIR}"/german.kbd "${TARGET_DIR}"/usr/share/de-latin1-nodeadkeys.kmap
